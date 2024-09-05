9e9b6b936f546c625f6a738ed08a1f7e6836ec97
NaufalGalang <galangpelo123@gmail.com>
2024-09-05 12:11:18 +0800

    Add files via upload

---
+++ soal-4/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Soal 4</title>
+</head>
+<body>
+    <h1>Soal 4: Hitung Umur Anda</h1>
+    <form action="hitung_umur.php" method="post">
+        <label for="birthdate">Masukkan Tanggal Lahir:</label>
+        <input type="date" id="birthdate" name="birthdate" required><br><br>
+
+        <input type="submit" value="Hitung Umur">
+    </form>
+</body>
+</html>
